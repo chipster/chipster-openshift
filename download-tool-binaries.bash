c7a6875867c28fe78e83b05eb82db0e57acced39
klemela <klemela>
2016-04-15 10:04:28 +0300

    Start from the Ubuntu 12.04 image

--- download-tool-binaries.bash
+++ download-tool-binaries.bash
@@ -13,6 +13,19 @@ spec:
       storage: 249G
 ' | oc create -f -
 
+echo '
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: tmp
+spec:
+  accessModes:
+  - ReadWriteMany
+  resources:
+    requests:
+      storage: 249G
+' | oc create -f -
+
 
 #oc delete job download
 TOOLS_URL="http://bio.nic.funet.fi/pub/sci/molbio/chipster/dist/virtual_machines/3.7.2/tools/tools.tar.gz"
@@ -37,19 +50,24 @@ spec:
         - name: volume-sjrxr
           persistentVolumeClaim:
             claimName: tool-binaries
+        - name: volume-sjrx2
+          persistentVolumeClaim:
+            claimName: tmp
       containers:
       - name: download
-        image: java:8
+        image: 172.30.1.144:5000/chipster/base
         # basically just "curl URL | tar -zx", but with some tweaks to get realtime output from curl:
         # - curl outputs to a named pipe so that it will not suppress the status updates to stderr
         # - redirect stderr to stdout for easier editing
         # - flush output buffer after each line (stdbuf -oL)
         # - curl prints a carriage return after each status line. Replace it with a new line character
-        command: ["bash", "-c", "df -h && cd /opt/chipster/tools && mkfifo /tmp/pipe && tar -zxf /tmp/pipe & curl '$TOOLS_URL' -o /tmp/pipe --keepalive-time 2 2>&1 | stdbuf -oL tr '\''\r'\'' '\''\n'\''"]
-        #command: ["bash", "-c", "df -h && cd /opt/chipster/tools && curl '$TOOLS_URL' -o tools.tar.gz 2>&1 | stdbuf -oL tr '\''\r'\'' '\''\n'\''"]
+        #command: ["bash", "-c", "df -h && cd /opt/chipster/tools && mkfifo /tmp/pipe && tar -zxf /tmp/pipe & curl '$TOOLS_URL' -o /tmp/pipe 2>&1 | stdbuf -oL tr '\''\r'\'' '\''\n'\''"]
+        command: ["bash", "-c", "df -h && curl '$TOOLS_URL' -o /opt/chipster/tmp/tools.tar.gz 2>&1 | stdbuf -oL tr '\''\r'\'' '\''\n'\'' && cd /opt/chipster/tools && cat /opt/chipster/tmp/tools.tar.gz | tar -zx"]
         volumeMounts:
            - name: volume-sjrxr
              mountPath: /opt/chipster/tools
+           - name: volume-sjrx2
+             mountPath: /opt/chipster/tmp             
       restartPolicy: Never
 ' | oc create -f -
 
