2152fcebf07925d20fd650f50ac3841d69b05590
klemela <klemela>
2016-04-25 12:20:35 +0300

    Fix the name of the comp startup class

--- utils/build-all.bash
+++ utils/build-all.bash
@@ -18,7 +18,9 @@ bash utils/create-image.bash dockerfiles/comp comp server
 
 
 
+oc delete dc artefacts \
+#&& oc delete $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1) \
+&& oc run artefacts --image 172.30.1.144:5000/chipster/comp --command sleep inf \
+&& oc set volume dc/artefacts --add -t pvc --mount-path /mnt/artefacts --claim-name artefacts
 
-# oc run artefacts --image 172.30.1.144:5000/chipster/comp --command sleep inf
-# oc set volume dc/artefacts --add -t pvc --mount-path /mnt/artefacts --claim-name artefacts
-# oc rsh $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1)
+oc rsh $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1)
