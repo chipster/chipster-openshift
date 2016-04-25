9685b158121efe369302f19e6c6ddfb7a7e78de1
klemela <klemela>
2016-04-25 12:51:34 +0300

    Separate entrypoint and cmd for the server Dockerfile

--- utils/build-all.bash
+++ utils/build-all.bash
@@ -17,10 +17,8 @@ bash utils/create-image.bash dockerfiles/server server
 bash utils/create-image.bash dockerfiles/comp comp server
 
 
-
-oc delete dc artefacts \
-#&& oc delete $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1) \
-&& oc run artefacts --image 172.30.1.144:5000/chipster/comp --command sleep inf \
-&& oc set volume dc/artefacts --add -t pvc --mount-path /mnt/artefacts --claim-name artefacts
-
-oc rsh $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1)
+# oc delete dc artefacts
+# oc delete $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1)
+# oc run artefacts --image 172.30.1.144:5000/chipster/comp --command sleep inf
+# oc set volume dc/artefacts --add -t pvc --mount-path /mnt/artefacts --claim-name artefacts
+# oc rsh $(oc get pod -l run=artefacts | grep -v NAME | cut -d " " -f 1)
