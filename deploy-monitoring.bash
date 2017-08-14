c6f775c208b9d96d5de1dbef90e9d5084532e972
klemela <klemela>
2017-08-14 15:52:56 +0300

    Working on monitoring

---
+++ deploy-monitoring.bash
@@ -0,0 +1,41 @@
+oc new-app --docker-image=influxdb
+
+oc set volume dc/influxdb --add --name influxdb -t pvc --mount-path /var/lib/influxdb --claim-name=influxdb --claim-size=4 --overwrite
+
+oc delete is grafana ; oc delete bc grafana ; oc new-build --name=grafana -D - < dockerfiles/grafana/Dockerfile --to grafana
+oc delete dc grafana ; oc delete service grafana ; oc new-app grafana
+
+//oc set volume dc/grafana --add -t emptyDir --mount-path /usr/share/grafana/data
+oc set volume dc/grafana --add --name grafana -t pvc --mount-path /usr/share/grafana/data --claim-name=grafana --claim-size=4 --overwrite
+oc expose dc grafana --port=3000
+oc expose service grafana
+
+
+# run in influxdb container
+# curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE db" -X POST
+
+# configure grafana:
+# Data source type: InfluxDB
+# Http Url: http://influxdb:8086
+# Access: proxy
+# Database: db
+
+# run in base container
+# git clone https://github.com/chipster/chipster-web-server.git
+# npm install
+# tsc
+# pushd ../type-service ; npm install ; tsc ; popd 
+# node src/chipster login web-server:8000 -u admin -p admin
+# client get's public addresses from the service locator, but can't connect  to them
+# wget https://github.com/openshift/origin/releases/download/v3.6.0/openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz
+# tar -zxf openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz
+# cd openshift
+#  ./oc login dac-oso.csc.fi:8443 --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt --token $(cat /run/secrets/kubernetes.io/serviceaccount/token )
+
+
+oc patch dc comp -p "$(cat script-utils/monitoring/resources.json)"
+oc patch dc comp -p "$(cat script-utils/monitoring/monitoring-container.json)"
+
+# oc delete  bc monitoring ; oc delete is monitoring
+oc new-build . --context-dir='.' --name=monitoring -D - < dockerfiles/monitoring/Dockerfile
+ 
---
+++ script-utils/monitoring/monitoring.bash
@@ -0,0 +1,31 @@
+influxdb="http://influxdb:8086"
+role="comp"
+auth_resp=$(curl -s -S -X POST http://auth:8002/tokens?pretty -u admin)
+echo auth response "$auth_resp"
+token=$(echo "$auth_resp" | grep tokenKey | cut -d '"' -f 4 )
+echo token $token
+while true; do
+
+  status=$(curl localhost:8012/admin/status?pretty -u token:${token} | grep ":")
+
+  echo status "$status"
+
+   echo "$status" | while read line; do
+
+    key=$(echo "$line" | grep : | cut -d '"' -f 2)
+    # xargs to trim whitespace
+    value=$(echo "$line" | grep : | cut -d ':' -f 2 | cut -d ',' -f 1 | xargs)
+
+    # if not a number, add quotes to store as a string
+    if [[ $value != *[[:digit:]]* ]]; then
+     value="\"$value\""
+    fi
+
+    db_key=$(echo ${key},role=${role},id=${HOSTNAME})
+
+    echo "** save ${db_key} $value"
+    curl -XPOST "${influxdb}/write?db=db" --data-binary "${db_key} value=$value"
+	done
+
+	sleep 10
+done
