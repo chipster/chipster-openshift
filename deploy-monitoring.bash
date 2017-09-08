d93de5f6f9da72272ad54015b02993ba824aba7a
klemela <klemela>
2017-09-08 17:02:14 +0300

    Fix the monitoring image url

--- deploy-monitoring.bash
+++ deploy-monitoring.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -z "$password" ]
+then
+      echo "Error: \$password is empty"
+      echo "Monitoring requires a Chipster user account."
+      echo "Create a new user called \"monitoring\" on the auth service and save "
+      echo "it's password to the env \$password before running this script."
+      exit 1 
+fi
+
 source script-utils/deploy-utils.bash
 
 # deploy Influxdb from the official image
@@ -36,10 +45,16 @@ for role in auth comp file-broker scheduler service-locator session-db session-w
 	echo $role
 
 	# configure lower resource limits to make space for the second container	
-	oc get $d -o json | jq '.spec.template.spec.containers[0].resources={ "limits": { "cpu": "1900m", "memory": "1Gi"}, "requests": { "cpu": "200m", "memory": "100Mi"}}' | oc replace $d -f -
+	oc get dc $role -o json | jq '.spec.template.spec.containers[0].resources={ "limits": { "cpu": "1900m", "memory": "1Gi"}, "requests": { "cpu": "200m", "memory": "100Mi"}}' | oc replace dc $role -f -
 	
 	# add the new container json to the deployment config
 	oc get dc $role -o json | jq '.spec.template.spec.containers[1]='"$(cat script-utils/monitoring/monitoring-container.json)" | oc replace dc $role -f -
+	
+	# get the image repo address 
+	image_repo=$(dirname $(oc get dc $role -o json | jq '.spec.template.spec.containers[0].image' -r ))
+	
+	# and configure the address of the monitoring image
+	oc get dc $role -o json | jq .spec.template.spec.containers[1].image=\"$image_repo/monitoring\" | oc replace dc $role -f -
 	
 	# get the admin port of this service from the config defaults	
 	admin_port=$(cat ../chipster-web-server/conf/chipster-defaults.yaml | grep url-admin-bind-$role | cut -d ":" -f 4)
--- redeploy-services.bash
+++ redeploy-services.bash
@@ -1,3 +1,8 @@
+# Redeploy manually all services having our code.
+# Most often the deployemnt happens after new code is pusehd to the repository. GitHub webhooks
+# can be configured to handle that automatically, but for some infrequent maintenance tasks
+# running this manually is fine, e.g. after running create-secrets.bash
+   
 for d in auth service-locator session-db file-broker scheduler comp session-worker type-service web-server toolbox; do 
 	oc deploy $d --latest
 done
