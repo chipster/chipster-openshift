974d0846dca4d1d1ab358b96aa2d293b2f8abb61
klemela <klemela>
2018-11-01 16:19:42 +0200

    Fix Postgres volumes

--- deploy-postgres.bash
+++ deploy-postgres.bash
@@ -20,7 +20,10 @@ oc new-app centos/postgresql-95-centos7 --name job-history-postgres \
     -e POSTGRESQL_DATABASE=job_history_db
     
 
-#oc volume dc/auth-postgres --remove --name postgres-data
+oc volume dc/auth-postgres --remove --name auth-postgres-volume-1
+oc volume dc/session-db-postgres --remove --name session-db-postgres-volume-1
+oc volume dc/job-history-postgres --remove --name job-history-postgres-volume-1
+sleep 5
 oc volume dc/auth-postgres        --add --name postgres-data --type=pvc --claim-name auth-postgres-data        --mount-path /var/lib/pgsql/data
 oc volume dc/session-db-postgres  --add --name postgres-data --type=pvc --claim-name session-db-postgres-data  --mount-path /var/lib/pgsql/data
 oc volume dc/job-history-postgres --add --name postgres-data --type=pvc --claim-name job-history-postgres-data --mount-path /var/lib/pgsql/data
