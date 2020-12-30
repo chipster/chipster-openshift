d9e936c6ef1052a3c761a5c418218ab45ba8ec17
Petri Klemela <petri.klemela@csc.fi>
2020-12-30 10:42:02 +0200

    Update to yq v4 syntax

--- k3s/generate-passwords.bash
+++ k3s/generate-passwords.bash
@@ -12,31 +12,36 @@ set -e
 
 # e30K is base64 encoded {}
 secret_template=$(cat << "EOF"
-apiVersion: v1
-kind: Secret
-metadata:
-    name: passwords
-    labels:
-        app: chipster
-type: Opaque
-data:
-    values.yaml: e30K
+{
+  "apiVersion": "v1",
+  "kind": "Secret",
+  "metadata": {
+    "name": "passwords",
+    "labels": {
+      "app": "chipster"
+    }
+  },
+  "type": "Opaque",
+  "data": {
+    "values.yaml": "e30K"
+  }
+}
 EOF
 )
 
 echo "get old passwords from the server"
-if ! secret_yaml=$(kubectl get secret passwords -o yaml); then
-   secret_yaml="$secret_template" 
+if ! secret_json=$(kubectl get secret passwords -o json); then
+   secret_json="$secret_template" 
 fi
 
-values_json=$(echo "$secret_yaml" | yq r - --tojson | jq '.data."values.yaml"' -r | base64 -d)
+values_json=$(echo "$secret_json" | jq '.data."values.yaml"' -r | base64 -d)
 
 default_values_yaml_path="helm/chipster/values.yaml"
 
 #echo "$values_json"
 
-for key in $(yq r $default_values_yaml_path --tojson | jq '.deployments | keys[]' -r); do
-    name=$(yq r $default_values_yaml_path --tojson | jq .deployments.$key.name -r)
+for key in $(yq e $default_values_yaml_path --tojson | jq '.deployments | keys[]' -r); do
+    name=$(yq e $default_values_yaml_path --tojson | jq .deployments.$key.name -r)
     old_password=$(echo "$values_json" | jq '.deployments."'$key'".password' -r)
     if [[ $old_password != "null" ]]; then
         echo "use old password for $name:   $(echo "$old_password" | cut -c1-5)..."
@@ -47,9 +52,9 @@ for key in $(yq r $default_values_yaml_path --tojson | jq '.deployments | keys[]
     fi
 done
 
-for key in $(yq r $default_values_yaml_path --tojson | jq '.databases | keys[]' -r); do
+for key in $(yq e $default_values_yaml_path --tojson | jq '.databases | keys[]' -r); do
     name="$key"
-    passwordKey=$(yq r $default_values_yaml_path --tojson | jq .databases.$key.passwordKey -r)
+    passwordKey=$(yq e $default_values_yaml_path --tojson | jq .databases.$key.passwordKey -r)
     old_password=$(echo "$values_json" | jq .$passwordKey -r)
     if [[ $old_password != "null" ]]; then
         echo "use old password for $name:   $(echo "$old_password" | cut -c1-5)..."
@@ -60,7 +65,7 @@ for key in $(yq r $default_values_yaml_path --tojson | jq '.databases | keys[]' 
     fi
 done
 
-for key in $(yq r $default_values_yaml_path --tojson | jq '.users | keys[]' -r); do
+for key in $(yq e $default_values_yaml_path --tojson | jq '.users | keys[]' -r); do
     name="$key"
     old_password=$(echo "$values_json" | jq '.users."'$key'".password' -r)
     if [[ $old_password != "null" ]]; then
@@ -72,7 +77,7 @@ for key in $(yq r $default_values_yaml_path --tojson | jq '.users | keys[]' -r);
     fi
 done
 
-for key in $(yq r $default_values_yaml_path --tojson | jq '.tokens | keys[]' -r); do
+for key in $(yq e $default_values_yaml_path --tojson | jq '.tokens | keys[]' -r); do
     name="$key"    
     old_password=$(echo "$values_json" | jq '.tokens."'$key'".privateKey' -r)
     if [[ $old_password != "null" ]]; then
@@ -88,5 +93,5 @@ done
 
 echo "update to server"
 
-echo "$secret_yaml" | yq r - --tojson | jq ".data.\"values.yaml\" = \"$(echo "$values_json" | base64)\"" \
+echo "$secret_json" | jq ".data.\"values.yaml\" = \"$(echo "$values_json" | base64)\"" \
     | kubectl apply -f -
