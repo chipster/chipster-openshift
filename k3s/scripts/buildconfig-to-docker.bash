0816ffc423279718d7f3b02b68e97bb5b4739f50
Petri Klemela <petri.klemela@csc.fi>
2021-03-26 08:53:44 +0000

    Use yq 4 in builconfig-to-docker

--- k3s/scripts/buildconfig-to-docker.bash
+++ k3s/scripts/buildconfig-to-docker.bash
@@ -34,27 +34,27 @@ build="$(basename $dir)"
 
 cmd="cat $dir/Dockerfile"
 
-uri=$(cat $dir/*.yaml | yq r - objects[0].spec.source.git.uri)
+uri=$(cat $dir/*.yaml | yq e .objects[0].spec.source.git.uri -)
 
-image_count=$(cat $dir/*.yaml | yq r - objects[0].spec.source.images --tojson | jq '. | length')
+image_count=$(cat $dir/*.yaml | yq e .objects[0].spec.source.images - --tojson | jq '. | length')
 
 if [[ $image_count != 0 ]]; then 
     last_index=$(($image_count - 1))
     for i in $(seq 0 $last_index); do
-        image=$(cat $dir/*.yaml | yq r - objects[0].spec.source.images[$i].from.name)
-        destination=$(cat $dir/*.yaml | yq r - objects[0].spec.source.images[$i].paths[0].destinationDir)
-        source=$(cat $dir/*.yaml | yq r - objects[0].spec.source.images[$i].paths[0].sourcePath)
+        image=$(cat $dir/*.yaml | yq e .objects[0].spec.source.images[$i].from.name -)
+        destination=$(cat $dir/*.yaml | yq e .objects[0].spec.source.images[$i].paths[0].destinationDir -)
+        source=$(cat $dir/*.yaml | yq e .objects[0].spec.source.images[$i].paths[0].sourcePath -)
         source_basename=$(basename $source)
         copy_line="$(cat $dir/Dockerfile | grep "COPY $destination")"
         copy_destination="$(echo "$copy_line" | cut -d " " -f 3)"
         docker_copy_line="COPY --from=$image $source $copy_destination/$source_basename"
 
-        # >&2 echo "copy from image:                      $image"
-        # >&2 echo "path:                                 $source"
-        # >&2 echo "to build context path:                $destination"
-        # >&2 echo "original COPY line in Dockerfile:     $copy_line"
-        # >&2 echo "copy destination:                     $copy_destination"
-        # >&2 echo "dockerized COPY line:                 $docker_copy_line"
+        >&2 echo "copy from image:                      $image"
+        >&2 echo "path:                                 $source"
+        >&2 echo "to build context path:                $destination"
+        >&2 echo "original COPY line in Dockerfile:     $copy_line"
+        >&2 echo "copy destination:                     $copy_destination"
+        >&2 echo "dockerized COPY line:                 $docker_copy_line"
         
 
         if [[ $source == *"mylly"* ]]; then
@@ -71,7 +71,7 @@ fi
 
 # cmd="$cmd | tee /dev/tty"
 
-if [[ -z $uri ]]; then
+if [ $uri = "null" ]; then
     cmd="$cmd | sudo docker build -t $build -"
 else
     cmd="$cmd | sudo docker build -t $build -f - $uri"
