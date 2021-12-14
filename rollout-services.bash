e958e079891c4fdc40607f236705eb7813235aa0
Taavi Hupponen <taavi.hupponen@csc.fi>
2021-12-14 13:00:58 +0200

    Add  stateful set restart note

--- rollout-services.bash
+++ rollout-services.bash
@@ -22,4 +22,8 @@ for d in auth service-locator session-db file-broker scheduler comp session-work
 done
 
 # is there a better way?
+
+# - alternative 1: scale to zero and then back in the yaml in web ui
+# - alternative 2: delete pods
+# - alternative 3: delete stateful set in web and recreate using deploy_servers.bash
 #json=$(oc get sts file-storage$subproject_postfix -o json); oc delete sts file-storage$subproject_postfix; echo "$json" | oc apply -f -
