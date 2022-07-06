6d4e9fa485ab6f9da34f20d1459024b202e02178
Petri Klemela <petri.klemela@csc.fi>
2022-07-06 12:44:44 +0300

    Branches are set in Kustomize now

--- deploy-all.bash
+++ deploy-all.bash
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-subproject="$1"
-
 # run only once, because we can't change the db password without deleting the db
-bash generate-passwords.bash $subproject
+bash generate-passwords.bash
 
 # run if configuration has changed (and bash rollout-services.bash if running only this)
-bash create-secrets.bash $subproject
+bash create-secrets.bash
 
 # deploy databases
-bash deploy-postgres.bash $subproject
+bash deploy-postgres.bash
 
 # create or update builds
-bash deploy-builds.bash master
+bash deploy-builds.bash
 
 # run if the templates have changed or there are new services
 # - remove all: bash remove-all-services.bash
-bash deploy-servers.bash $subproject
+bash deploy-servers.bash
 
 # optional
 #bash deploy-mylly-app.bash
@@ -25,7 +23,7 @@ bash deploy-servers.bash $subproject
 #bash rollout-services.bash
 
 # create default users in auth and configure grafana password and dashboards (not started with the default quota)
-bash setup.bash $subproject
+bash setup.bash
 
 # download tools if you have enough storage quota
 #bash download-tools-bin.bash
