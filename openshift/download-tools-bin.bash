f5c8ab5ef1addc86d130883941e1a786212ea3c7
Petri Klemela <petri.klemela@csc.fi>
2023-11-27 16:21:33 +0200

    Move tools-bin download scripts to openshift dir

--- openshift/download-tools-bin.bash
+++ openshift/download-tools-bin.bash
@@ -4,7 +4,7 @@ set -e
 
 command -v yq >/dev/null 2>&1 || { echo >&2 "I require yq but it's not installed.  Aborting."; exit 1; }
 
-source scripts/utils.bash
+source ../scripts/utils.bash
 
 tools_bin_version="$1"
 tools_bin_size="$2"
@@ -26,32 +26,16 @@ if oc get pvc $pvc_name; then
   exit 1
 fi
 
-oc process -f templates/jobs/pvc.yaml --local \
+echo "** create pvc"
+
+oc process -f jobs/pvc.yaml --local \
 	-p NAME=$pvc_name \
 	-p SIZE=$tools_bin_size \
   -p STORAGE_CLASS="glusterfs-storage" \
 	| oc create -f - --validate
 	
-# temp_pvc="${pvc_name}-temp"
-# while oc get pvc $temp_pvc; do
-#   oc delete pvc $temp_pvc
-#   sleep 1
-# done
-
-# oc process -f templates/jobs/pvc.yaml --local \
-# 	-p NAME=$temp_pvc \
-# 	-p SIZE=400Gi \
-#   -p STORAGE_CLASS="standard-rwo" \
-# 	| oc create -f - --validate
-
-# oc process -f templates/jobs/pvc.yaml --local \
-# 	-p NAME=$temp_pvc \
-# 	-p SIZE=400Gi \
-#   -p STORAGE_CLASS="glusterfs-storage" \
-# 	| oc create -f - --validate
-
 # bash run-job-with-tools-bin.bash "templates/jobs/download-tools-bin.bash" "$tools_bin_version" "$temp_pvc"
-bash run-job-with-tools-bin.bash "templates/jobs/download-tools-bin.bash" "$tools_bin_version"
+bash jobs/run-job-with-tools-bin.bash "jobs/download-tools-bin.bash" "$tools_bin_version"
 
 #TODO how to run this after the job has finished?
 #oc delete pvc $temp_pvc
