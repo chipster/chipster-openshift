5fbf7568b9b0ad877ae12043a432997d0f337d3f
Petri Klemela <petri.klemela@csc.fi>
2023-12-05 10:10:51 +0200

    Move some scripts to openshift dir

--- openshift/rollout-services.bash
+++ openshift/rollout-services.bash
@@ -1,7 +1,7 @@
 # Redeploy manually all services having our code.
 # Most often the deployemnt happens after new code is pusehd to the repository. GitHub webhooks
 # can be configured to handle that automatically, but for some infrequent maintenance tasks
-# running this manually is fine, e.g. after running create-secrets.bash
+# running this manually is fine
 
 subproject="$1"
 
