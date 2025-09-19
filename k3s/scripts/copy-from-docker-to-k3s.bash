655673cb162781967f6ddaeddbeff6933f0a0a0c
Petri Klemela <petri.klemela@csc.fi>
2025-09-19 08:11:18 +0300

    Replace Bitnami chart
    Squashed commit of the following:
    
    commit 807e97734f509963d976403433432cd6da145e10
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Thu Sep 18 14:52:27 2025 +0000
    
        Fix typos
    
    commit 02e6b75c97817c46ef7d2a26067e065b0db8f0df
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Thu Sep 18 14:51:52 2025 +0000
    
        Fix typo
    
    commit cc7e252fd8183be1e0ea69f7b146f69e32053406
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Thu Sep 18 14:50:05 2025 +0000
    
        Add version
    
    commit 1da1bb0c320816acf0d1ed6b0dee499a33a46054
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Thu Sep 18 14:48:00 2025 +0000
    
        Fix link
    
    commit 7fe2a8a2429ce67555ed1db400290d84aec8195a
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Thu Sep 18 14:46:04 2025 +0000
    
        Bitnami update instructions
    
    commit e7d352c1f9c24c3bd1c74c34bb68ca3b209ae988
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Tue Sep 16 11:56:52 2025 +0000
    
        Use initdb to generate conf
    
    commit 43ab92781da56d80ffeee7ef1e580be2491b0bf6
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Tue Sep 16 04:46:58 2025 +0000
    
        Fix filebeat url
    
    commit 79fa892a299857a1eadcd11b388b279a1e407fb9
    Author: Petri Klemela <petri.klemela@csc.fi>
    Date:   Mon Sep 15 14:13:53 2025 +0000
    
        Replace Bitnami chart

--- k3s/scripts/copy-from-docker-to-k3s.bash
+++ k3s/scripts/copy-from-docker-to-k3s.bash
@@ -7,8 +7,25 @@ if [ -z $1 ]; then
   exit 1
 fi
 
-image="$1"
+source_repo="image-registry.apps.2.rahti.csc.fi/chipster-images/"
+source_image="$1"
+source_tag="latest"
+target_repo="$(cat ~/values.yaml | yq e -o json | jq -r '.image.chipsterImageRepo')"
+target_image="$source_image"
+target_tag="$(cat ~/values.yaml | yq e -o json | jq -r '.image.tag')"
 
-time sudo docker run --privileged --volume /var/run:/var/run quay.io/skopeo/stable:latest copy \
-    docker-daemon:image-registry.apps.2.rahti.csc.fi/chipster-images/$image:latest \
-    containers-storage:image-registry.apps.2.rahti.csc.fi/chipster-images/$image:latest
+echo "tag in Docker"
+
+echo "   $source_repo$source_image:$source_tag"
+echo "as $target_repo$target_image:$target_tag"
+
+sudo docker tag $source_repo$source_image:$source_tag $target_repo$target_image:$target_tag
+
+echo "copy from Docker to K3s"
+
+sudo docker save $target_repo$target_image:$target_tag | sudo k3s ctr -n k8s.io images import -
+  
+# 2025-09-15: copied images are not visible in K3s
+# time sudo docker run --privileged --volume /var/run:/var/run quay.io/skopeo/stable:latest copy \
+#     docker-daemon:$source_repo$source_image:$source_tag \
+#     containers-storage:$target_repo$target_image:$target_tag
